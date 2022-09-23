1d4153bd180bca3e8deaa8576652677709dd03d7
kjma8y <tlsrudghks@gmail.com>
2022-09-23 21:28:20 +0900

    ui 멈춤 해결

--- JPlearning.py
+++ JPlearning.py
@@ -8,8 +8,47 @@ from PyQt5.QtCore import *
 from PyQt5 import uic
 from cutNresize import cutNresize
 from train import train_module
+from detect2 import detectNcut
 
-form_learning = uic.loadUiType("Third.ui")[0]
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+form_learning = uic.loadUiType(BASE_DIR +  '/' +"Third.ui")[0]
+
+
+class ThreadLearn(QThread):
+    user_signal = pyqtSignal(str, str)
+
+    def __init__(self, parent, cha_name, src, dst, level):
+        super().__init__(parent)
+        self.folder_source = src
+        self.folder_destination = dst
+        self.chara_name = cha_name
+        self.learning_level = level
+
+    def run(self):
+        if os.path.exists(self.folder_source) and os.path.exists(self.folder_destination) and self.chara_name != "":
+            img_path = self.folder_source
+            tmpCropDir = self.folder_source + '/' + 'tmpCrop'
+            # cutNresize(img_path, tmpCropDir)
+            detectNcut(img_path, tmpCropDir)
+            char_dir = tmpCropDir + '/' + '0'
+            other_dir = tmpCropDir + '/' + '1'
+            if not os.path.exists(char_dir):
+                os.mkdir(char_dir)
+            if not os.path.exists(other_dir):
+                os.mkdir(other_dir)
+            self.user_signal.emit('전처리 완료', tmpCropDir + '\n에 있는 이미지를 폴더에 옮겨 담기 바랍니다.'
+                                  '\n대상 캐릭터는 0, 대상 캐릭터 외는 1입니다. \n남는 이미지가 없도록 하십시오.')
+            model_path = self.folder_destination + '/' + self.chara_name
+            if not os.path.exists(model_path):
+                os.mkdir(model_path)
+            accuracy_rate = train_module(tmpCropDir, self.learning_level, model_path)
+            f = open(model_path + '/' + self.chara_name + '.txt', 'w')
+            f.close()
+            self.user_signal.emit('학습 완료', '학습된 데이터의 학습 셋에 대한 정확도: ' + str(
+                accuracy_rate) + '\n학습 데이터의 경로는\n' + model_path + '\n입니다.')
+        else:
+            self.user_signal.emit('error', '경로와 이름 설정을 다시 한 번 확인해주세요.')
+
 
 class LearningWindow(QDialog, QWidget, form_learning):
     def __init__(self):
@@ -59,33 +98,20 @@ class LearningWindow(QDialog, QWidget, form_learning):
         self.chara_name = self.LineNameInput.text()
         self.LabelNameInput.setText(self.chara_name)
 
-    def start_train(self):
-        if os.path.exists(self.folder_source) and os.path.exists(self.folder_destination) and self.chara_name != "":
-            img_path = self.folder_source
-            tmpCropDir = self.folder_source + '/' + 'tmpCrop'
-            cutNresize(img_path, tmpCropDir)
-            char_dir = tmpCropDir + '/' + '0'
-            other_dir = tmpCropDir + '/' + '1'
-            if not os.path.exists(char_dir):
-                os.mkdir(char_dir)
-            if not os.path.exists(other_dir):
-                os.mkdir(other_dir)
-            QMessageBox.about(self, '전처리 완료',
-                              tmpCropDir + '\n에 있는 이미지를 폴더에 옮겨 담기 바랍니다.\n대상 캐릭터는 0, 대상 캐릭터 외는 1입니다. \n남는 이미지가 없도록 하십시오.')
-            model_path = self.folder_destination + '/' + self.chara_name
-            if not os.path.exists(model_path):
-                os.mkdir(model_path)
-            accuracy_rate = train_module(tmpCropDir, self.learning_level, model_path)
-            f = open(model_path + '/' + self.chara_name + '.txt', 'w')
-            f.close()
-            QMessageBox.about(self, '학습 완료', '학습된 데이터의 학습 셋에 대한 정확도: ' + str(
-                accuracy_rate) + '\n학습 데이터의 경로는\n' + model_path + '\n입니다.')
-        else:
-            QMessageBox.about(self, 'error', '경로와 이름 설정을 다시 한 번 확인해주세요.')
-
     def change_slider_value(self, value):
         self.learning_level = value
         self.LabelValueSlider.setText(str(value))
+
+    def start_train(self):
+        self.BtnRun.setEnabled(False)
+        Thread1 = ThreadLearn(self, self.chara_name, self.folder_source, self.folder_destination, self.learning_level)
+        Thread1.user_signal.connect(self.slot_do)
+        Thread1.start()
+
+    def slot_do(self, title, message):
+        QMessageBox.about(self, title, message)
+        self.BtnRun.setEnabled(True)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
