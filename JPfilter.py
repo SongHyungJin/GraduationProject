a8f680f95d804802990dd2968f802f1b49008f46
kjma8y <tlsrudghks@gmail.com>
2022-09-23 21:28:46 +0900

    ui 멈춤 해결

--- JPfilter.py
+++ JPfilter.py
@@ -3,6 +3,13 @@
 
 import os
 import sys
+
+try:
+    os.chdir(sys._MEIPASS)
+    print(sys._MEIPASS)
+except:
+    os.chdir(os.getcwd())
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -12,7 +19,103 @@ from numpy import dot
 from numpy.linalg import norm
 import numpy as np
 
-form_filter = uic.loadUiType("Second.ui")[0]
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+form_filter = uic.loadUiType(BASE_DIR +  '/' +"Second.ui")[0]
+
+
+class ThreadFilter(QThread):
+    user_signal = pyqtSignal(str, str)
+
+    def __init__(self, parent, curFolder, lstRa, idxRa, lstRs, idxRs):
+        super().__init__(parent)
+        self.parent = parent
+        self.currentFolder = curFolder
+        self.listRate = lstRa
+        self.idxRate = idxRa
+        self.listResize = lstRs
+        self.idxResize = idxRs
+
+    def run(self):
+        self.filter_Image()
+
+    def filter_Image(self):
+        # 이미지 로드
+        folder = self.currentFolder
+        vec_imgs = self.load_File(folder)
+
+        # 이미지 벡터들에 대해 코사인 유사도 측정 후 처리
+        # loc:유사도를 측정할 다른 한 쪽의 파일의 vec_images 내 위치
+        # origin:0~size-1, 여기서 pop된 값을 target으로, 처리가 끝나면 target을 인덱스로 파일 리스트에서 일괄 이동
+        # 격리 시 vec_images에서 제거, loc은 유지(loc이 가리키던 파일은 이미 중복으로 판단되었고 다시 판별할 필요 X)
+        origin = []
+        targets = []
+        cnt = 0  # 큰 루프 돈 횟수
+        for i in range(0, len(vec_imgs)):
+            origin.append(i)
+        for vec_img in vec_imgs:
+            loc = cnt + 1
+            if loc >= len(vec_imgs):
+                break
+            while loc < len(vec_imgs):
+                similarity = self.get_Cos_Sim(vec_img, vec_imgs[loc])
+                if similarity > self.listRate[self.idxRate]:
+                    targets.append(origin.pop(loc))
+                    del vec_imgs[loc]
+                else:
+                    loc = loc + 1
+            cnt = cnt + 1
+
+        # 파일 한 번에 이동
+        self.relocate_Image(folder, targets)
+        # 격리된 이미지 show OR 완료 메시지
+        self.fin_filter()
+
+    # 파일 로드 & 전처리
+    def load_File(self, folder):
+        filenames = os.listdir(folder)
+        new_size = self.listResize[self.idxResize]  # 이미지 리사이징 옵션
+        vec_imgs = []  # 이미지 로딩 & 전처리 후 리턴할 결과물
+
+        for filename in filenames:  # 아래 확장자에 해당하는 파일만 열기-PIL로 바꾸고 변동 가능성**
+            if filename.endswith(".bmp") or filename.endswith(".jpg") or filename.endswith(
+                    ".jpeg") or filename.endswith(".png"):  # or filename.endswith(".webp"):
+                with open(folder + '/' + filename, "rb") as file:
+                    img = Image.open(file)  # 이미지 파일 열기
+                    img = img.convert("RGB")
+                    img = img.resize((new_size, new_size))  # 이미지 사이즈 조정
+                    vec_img = self.vector_Image(img)  # 이미지 벡터화
+                    vec_img = vec_img.astype("float") / 256
+                    vec_imgs.append(vec_img)
+        return vec_imgs
+
+    # 이미지 벡터화
+    def vector_Image(self, img):
+        vec = np.asarray(img)  # 배열 형태로 전환
+        vec = vec.ravel()  # 3차원 -> 1차원 배열로 변환
+        return vec
+
+    # 코사인 유사도 측정
+    def get_Cos_Sim(self, vec1, vec2):
+        return dot(vec1, vec2) / (norm(vec1) * norm(vec2))
+
+    # 파일 이동
+    def relocate_Image(self, folder, targets):
+        img_src = folder
+        img_dest = folder + '/중복'
+        if not os.path.exists(img_dest):
+            os.makedirs(img_dest)
+        img_list = []
+        files = os.listdir(img_src)
+        for file in files:
+            if file.endswith(".bmp") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
+                img_list.append(file)
+        for target in targets:
+            os.replace(img_src + '/' + img_list[target], img_dest + '/' + img_list[target])
+
+    # 완료 알림
+    def fin_filter(self):
+        self.user_signal.emit('알림', '작업 완료')
+
 
 class FilterWindow(QDialog, QWidget, form_filter):
 
@@ -46,7 +149,7 @@ class FilterWindow(QDialog, QWidget, form_filter):
         # # #self.BtnExclude.clicked.connect(self.)
 
         # 중복 처리 작업 실행
-        self.BtnRun.clicked.connect(self.filter_Image)
+        self.BtnRun.clicked.connect(self.filter_start)
 
         # 이미지 축소율 변경
         self.OpResize_0.toggled.connect(lambda:self.setOpResize(0))
@@ -73,6 +176,7 @@ class FilterWindow(QDialog, QWidget, form_filter):
             self.currentFolder = folder
 
     # 이미지 출력
+    '''
     #def show_Image(self, images):
         #self.qPixmapFileVar = QPixmap()
         #self.qPixmapFileVar.load()
@@ -81,81 +185,17 @@ class FilterWindow(QDialog, QWidget, form_filter):
         #for image in images:
             #pixmap = QPixmap(image)
             #self.label_pic.setPixmap(pixmap)
+    '''
+
+    def filter_start(self):
+        self.BtnRun.setEnabled(False)
+        Thread1 = ThreadFilter(self, self.currentFolder, self.listRate, self.idxRate, self.listResize, self.idxResize)
+        Thread1.user_signal.connect(self.slot_do)
+        Thread1.start()
+
+    def slot_do(self, title, message):
+        QMessageBox.about(self, title, message)
+        self.BtnRun.setEnabled(True)
 
     # 중복 이미지 격리
-    def filter_Image(self):
-        # 이미지 로드
-        folder = self.currentFolder
-        vec_imgs = self.load_File(folder)
 
-        # 이미지 벡터들에 대해 코사인 유사도 측정 후 처리
-        # loc:유사도를 측정할 다른 한 쪽의 파일의 vec_images 내 위치
-        # origin:0~size-1, 여기서 pop된 값을 target으로, 처리가 끝나면 target을 인덱스로 파일 리스트에서 일괄 이동
-        # 격리 시 vec_images에서 제거, loc은 유지(loc이 가리키던 파일은 이미 중복으로 판단되었고 다시 판별할 필요 X)
-        origin = []
-        targets = []
-        cnt = 0 # 큰 루프 돈 횟수
-        for i in range(0, len(vec_imgs)):
-            origin.append(i)
-        for vec_img in vec_imgs:
-            loc = cnt+1
-            if(loc >= len(vec_imgs)):
-                break
-            while(loc < len(vec_imgs)):
-                similarity = self.get_Cos_Sim(vec_img, vec_imgs[loc])
-                if(similarity > self.listRate[self.idxRate]):
-                    targets.append(origin.pop(loc))
-                    del vec_imgs[loc]
-                else:
-                    loc = loc+1
-            cnt = cnt+1
-
-        # 파일 한 번에 이동
-        self.relocate_Image(folder, targets)
-        # 격리된 이미지 show OR 완료 메시지
-        self.fin_filter()
-
-    # 파일 로드 & 전처리
-    def load_File(self, folder):
-        filenames = os.listdir(folder)
-        new_size = self.listResize[self.idxResize]  # 이미지 리사이징 옵션
-        vec_imgs = []                               # 이미지 로딩 & 전처리 후 리턴할 결과물
-        
-        for filename in filenames:  # 아래 확장자에 해당하는 파일만 열기-PIL로 바꾸고 변동 가능성**
-            if filename.endswith(".bmp") or filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):# or filename.endswith(".webp"):
-                with open(folder + '/' + filename, "rb") as file:
-                    img = Image.open(file)                  # 이미지 파일 열기
-                    img = img.convert("RGB")
-                    img = img.resize((new_size, new_size))  # 이미지 사이즈 조정
-                    vec_img = self.vector_Image(img)        # 이미지 벡터화
-                    vec_img = vec_img.astype("float") / 256
-                    vec_imgs.append(vec_img)
-        return vec_imgs
-
-    # 이미지 벡터화
-    def vector_Image(self, img):
-        vec = np.asarray(img)   # 배열 형태로 전환
-        vec = vec.ravel()       # 3차원 -> 1차원 배열로 변환
-        return vec
-
-    # 코사인 유사도 측정
-    def get_Cos_Sim(self, vec1, vec2):
-        return dot(vec1, vec2)/(norm(vec1)*norm(vec2))
-
-    # 파일 이동
-    def relocate_Image(self, folder, targets):
-        img_src = folder
-        img_dest = folder+'/중복'
-        if not os.path.exists(img_dest):
-            os.makedirs(img_dest)
-        img_list = []
-        files = os.listdir(img_src)
-        for file in files:
-            if file.endswith(".bmp") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".png"):
-                img_list.append(file)
-        for target in targets:
-            os.replace(img_src+'/'+img_list[target], img_dest+'/'+img_list[target])
-
-    # 완료 알림
-    def fin_filter(self):
-        QMessageBox.about(self, '알림', '작업 완료')
